a21b79139db29e673f6867a15caf3e826863e843
Naveen-Pratap <naveennair1998@gmail.com>
2024-07-13 16:57:27 +0530

    Add test project + tests

---
+++ ExpenseTracker.Tests/Components/ExpenseItemTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ExpenseTracker.Components;
+
+namespace ExpenseTracker.Tests.Components
+{
+
+    public class ExpenseItemTests : TestContext
+    {
+        [Fact]
+        public void ExpenseItem_DisplaysCorrectInfo()
+        {
+            var tag = new ExpenseTag
+            {
+                Id = 1,
+                Name = "dummy"
+            };
+
+            var expense = new Expense
+            {
+                Id = 1,
+                Value = 100,
+                Description = "Test",
+                CreatedTime = DateTime.Now,
+                Tag = tag,
+            };
+
+            var cut = RenderComponent<ExpenseItem>(parameters => parameters.Add(p => p.expense, expense));
+            var valueElem = cut.Find(".value");
+            var descriptionElem = cut.Find(".description");
+            var createdTimeElem = cut.Find(".created-time");
+
+            Assert.Equal(expense.Value.ToString(), valueElem.TextContent.Trim());
+            Assert.Equal(expense.Description.ToString(), descriptionElem.TextContent.Trim());
+            Assert.Equal(expense.CreatedTime.ToString(), createdTimeElem.TextContent.Trim());
+        }
+
+    }
+}
---
+++ ExpenseTracker.Tests/Components/ExpenseListTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using ExpenseTracker.Components;
+
+namespace ExpenseTracker.Tests.Components
+{
+
+    public class ExpenseListsTests : TestContext
+    {
+        [Fact]
+        public void ExpenseList_DisplaysCorrectInfo()
+        {
+            var tag = new ExpenseTag
+            {
+                Id = 1,
+                Name = "dummy"
+            };
+
+            var expenses = new List<Expense>
+            {
+                new Expense { Id = 1, Value = 100, Description = "Expense 1", Tag = tag, CreatedTime = DateTime.Now },
+                new Expense { Id = 2, Value = 200, Description = "Expense 2", Tag = tag, CreatedTime = DateTime.Now }
+            };
+
+            var cut = RenderComponent<ExpenseList>(parameters => parameters.Add(p => p.expenses, expenses));
+
+            for (var i = 0; i < expenses.Count; i++)
+            {
+                var valueElem = cut.FindAll(".value")[i];
+                var descriptionElem = cut.FindAll(".description")[i];
+                var createdTimeElem = cut.FindAll(".created-time")[i];
+
+                Assert.Equal(expenses[i].Value.ToString(), valueElem.TextContent.Trim());
+                Assert.Equal(expenses[i].Description.ToString(), descriptionElem.TextContent.Trim());
+                Assert.Equal(expenses[i].CreatedTime.ToString(), createdTimeElem.TextContent.Trim());
+            }
+
+        }
+
+    }
+}
