ddb571107cb2463bf173e94898a005f4d3d7d3a3
Naveen-Pratap <naveennair1998@gmail.com>
2024-08-01 00:01:36 +0530

    make description and tag for expenses  mandatory

--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -12,9 +12,11 @@ namespace ExpenseTracker
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Please enter a valid cost > 0")]
         public int Value { get; set; }
+        [Required]
         public string Description { get; set; }
         public ExpenseTag Tag { get; set; }
 
+        [Required]
         public int Tagid { get; set; }
 
         public DateTime CreatedTime { get; set; }
